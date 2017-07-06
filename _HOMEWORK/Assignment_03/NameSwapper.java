4826d300179e80b5d6c475555649bb5f210600af
Ravi S. Rāmphal <rramphal@gmail.com>
2017-07-06 16:24:38 -0700

    added documentation

--- _HOMEWORK/Assignment_03/NameSwapper.java
+++ _HOMEWORK/Assignment_03/NameSwapper.java
@@ -1,15 +1,60 @@
+/**
+ * This class allows the user to process a datafile to fix incorrectly inputted
+ * names. It assumes a file format where headers (which remain untouched) are
+ * marked with pound signs at the beginning of the line. Blank lines are
+ * maintained. The only lines which are changed are those containing names in
+ * the format `firstname,lastname` which are then corrected to the format
+ * `lastname, firstname`.
+ *
+ * The user can call the program WITHOUT command-line arguments which will
+ * default to the input filename `datafile` and output filename `datafile.fix`.
+ * The program can be modified in the future to get rid of hard-coded filenames
+ * and instead ask for the filenames.
+ *
+ * The user can also call the program WITH command-line arguments where the
+ * first is the name of the file that is to be processed and the second is that
+ * of the file that is to be outputted to.
+ *
+ * NOTE: This program is not destructive and will die if the output file
+ * already exists and will not overwrite files. It leaves the input file intact.
+ *
+ * @name    NameSwapper
+ * @author  Ravi S. Ramphal
+ * @class   CCSF CS111B
+ * @date    2017.07.06
+ * @version 1.0
+ */
+
 import java.io.*;
 import java.util.*;
 
-public class NameSwapper
+public final class NameSwapper
 {
-    private static void die(String message)
+    /**
+     * This is a die method that prints a message to the stderr stream and
+     * exits the program with an error code.
+     *
+     * @param  errorMessage  The error message to be displayed to users
+     */
+    private static void die(String errorMessage)
     {
         System.err.println(errorMessage);
         System.exit(1);
     }
 
-    public static File validateInput(String filename)
+    /**
+     * This method validates the input file for the program to work with and
+     * returns its `File` object. It will cause the program to die if:
+     *     - the input name is not provided
+     *     - the file does not exist exists
+     *     - the file is not a file
+     *     - the file is a directory
+     *     - the file does exist and is a file, but cannot be read
+     *
+     * @param  filename  A string containing the name of the input file
+     * @return File      The File object of the file with the name provided
+     */
+    private static File validateInput(String filename)
     {
         if (filename.length() < 1) die("Input filename not provided!");
 
@@ -23,7 +68,18 @@ public class NameSwapper
         return file;
     }
 
-    public static File createOutput(String filename)
+    /**
+     * This method creates the output file for the program to work with and
+     * returns its `File` object. It will cause the program to die if:
+     *     - the output name is not provided
+     *     - the file already exists
+     *     - the file could not be created
+     *     - the file could be created, but not written to
+     *
+     * @param  filename  A string containing the name of the desired output file
+     * @return File      The File object of the file with the name provided
+     */
+    private static File createOutput(String filename)
     {
         if (filename.length() < 1) die("Output filename not provided!");
 
@@ -45,14 +101,35 @@ public class NameSwapper
         return file;
     }
 
-    public static boolean isHeader(String line)
+    /**
+     * The method checks whether or not the line is a header.
+     * It defaults to `false` and will return `false` on an empty string.
+     * Otherwise, if the line begins with a pound sign, it returns `true`.
+     *
+     * @param  line    The string containing the line to be processed
+     * @return boolean A boolean value as to whether the line is a header or not
+     */
+    private static boolean isHeader(String line)
     {
         if (line.length() == 0) return false;
         if (line.charAt(0) == '#') return true;
         return false;
     }
 
-    public static String processLine(String line)
+    /**
+     * This method is responsible for processing each line.
+     *
+     * The line is returned unmodified if:
+     *     - it is a header
+     *     - there are no commas
+     *     - there is more than one comma
+     *
+     * Otherwise, the line is returned swapped with a comma and a space between.
+     *
+     * @param  line   The string containing the line to be processed
+     * @return String The processed string
+     */
+    private static String processLine(String line)
     {
         if (isHeader(line)) return line;          // do not change header
         if (line.indexOf(",") == -1) return line; // only change comma lines
@@ -64,7 +141,18 @@ public class NameSwapper
         return parts[1] + ", " + parts[0];
     }
 
-    public static void processFile(File inputFile, File outputFile)
+    /**
+     * This method takes in the two files (already validated). Technically,
+     * error handling is not necessary in this method since the input and
+     * output files would have already been validated in the main method.
+     *
+     * A `Scanner` object is created to iterate over each line of the input file
+     * and each line is processed and then written to the output file.
+     *
+     * @param inputFile  The File that is to be processed
+     * @param outputFile The File that is to be outputted to
+     */
+    private static void processFile(File inputFile, File outputFile)
     {
         try
         {
@@ -93,6 +181,13 @@ public class NameSwapper
         }
     }
 
+    /**
+     * This is the `main` method of this class. It sets up default filenames
+     * but also allows for names to be passed in through command-line arguments.
+     * File validation is done here and then the file is processed.
+     *
+     * @param array An array of arguments provided to the program
+     */
     public static void main (String ... args)
     {
         String input  = "datafile";
