4b0632916ba676803471cea9a76a7879c04e688f
Ravi S. Rāmphal <rramphal@gmail.com>
2017-07-26 20:26:07 -0700

    generate output files for extra credit 2

---
+++ _HOMEWORK/Extra_Credit_02/printSourceCode.sh
@@ -0,0 +1,1 @@
+enscript --fancy-header --portrait --columns=1 --font=Courier@10 --line-numbers -p - --word-wrap --highlight=java --color=1 DrawShapes.java | pstopdf -i -o DrawShapes.pdf && open DrawShapes.pdf
