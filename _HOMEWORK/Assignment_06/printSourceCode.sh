2c76a1fe6845f52553f039abadcfa006c857878c
Ravi S. Rāmphal <rramphal@gmail.com>
2017-07-29 01:03:30 -0700

    completed homework assignment 6

---
+++ _HOMEWORK/Assignment_06/printSourceCode.sh
@@ -0,0 +1,1 @@
+enscript --fancy-header --portrait --columns=1 --font=Courier@10 --line-numbers -p - --word-wrap --highlight=java --color=1 ColorFactory.java | pstopdf -i -o ColorFactory.pdf && open ColorFactory.pdf
